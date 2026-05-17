bffafc9962ed9f97f41db8f978995a96800cf61d
AlexxxQ <152021684+AlexxxQ@users.noreply.github.com>
2026-05-17 12:37:49 +0300

    Add ambush AI behavior for monkey entity

--- TombEngine/Objects/TR3/Entity/tr3_monkey.cpp
+++ TombEngine/Objects/TR3/Entity/tr3_monkey.cpp
@@ -174,7 +174,15 @@ namespace TEN::Entities::Creatures::TR3
 		}
 		else
 		{
-			GetAITarget(creature);
+			if (item->AIBits & AMBUSH)
+			{
+				if (creature->Enemy == nullptr || creature->Enemy->ObjectNumber != ID_AI_AMBUSH)
+					FindAITargetObject(creature, ID_AI_AMBUSH, 0, true);
+			}
+			else
+			{
+				GetAITarget(creature);
+			}
 
 			if (creature->HurtByLara)
 				creature->Enemy = LaraItem;
